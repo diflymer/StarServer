f4cb8a215683745c23c66ae03e1af0f57dd53ec9
Danil <dan-kull@mail.ru>
2025-02-22 20:09:35 +0300

    ready server with nicks

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -2,9 +2,14 @@ import { Room, Client } from "@colyseus/core";
 import { MyRoomState } from "./schema/MyRoomState";
 import { MyState, Player } from "./MyState";
 
+import Phaser from "phaser"; // Подключаем Phaser
+import Matter from "matter-js"; // Подключаем Matter.js
+
 export class MyRoom extends Room<MyState> {
-  maxClients = 4;
-  //state = new MyRoomState();
+  maxClients = 8;
+  countClients = 0;
+
+  names = ['Истинный', 'Звезда', 'Огонёк', 'Водяной', 'Петух', 'Тюремщик', 'Водка', 'Коллайдер']
 
   state = new MyState();
 
@@ -12,7 +17,6 @@ export class MyRoom extends Room<MyState> {
     this.onMessage(0, (client, payload) => {
       // get reference to the player who sent the message
       const player = this.state.players.get(client.sessionId);
-      const velocity = 2;
 
       player.x = payload.x;
       player.y = payload.y;
@@ -21,6 +25,7 @@ export class MyRoom extends Room<MyState> {
 
 
   onJoin(client: Client, options: any) {
+    this.countClients += 1;
     console.log(client.sessionId, "joined!");
 
 
@@ -33,6 +38,14 @@ export class MyRoom extends Room<MyState> {
     // place Player at a random position
     player.x = (Math.random() * mapWidth);
     player.y = (Math.random() * mapHeight);
+    let name;
+    if (this.countClients <= this.names.length) {
+      name = this.names[this.countClients - 1];
+    } else {
+      name = this.names[Math.floor(Math.random() * this.names.length)];
+    }
+
+    player.name = name;
 
     // place player in the map of players by its sessionId
     // (client.sessionId is unique per connection!)
@@ -40,6 +53,7 @@ export class MyRoom extends Room<MyState> {
   }
 
   onLeave(client: Client, consented: boolean) {
+    this.countClients -= 1;
     console.log(client.sessionId, "left!");
 
     this.state.players.delete(client.sessionId);
--- src/rooms/MyState.ts
+++ src/rooms/MyState.ts
@@ -3,6 +3,7 @@ import { Schema, MapSchema, type } from "@colyseus/schema";
 export class Player extends Schema {
     @type("number") x: number;
     @type("number") y: number;
+    @type("string") name: string;
 }
  
 export class MyState extends Schema {
